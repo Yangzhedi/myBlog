f495d8e810998118f0bd33c18e8557364cf7711e
Yangzhedi <uiryzd@163.com>
2017-02-12 18:00:28 +0800

    blog 改为 Markdown显示

--- blog/models.py
+++ blog/models.py
@@ -5,21 +5,21 @@ from django.db import models
 from django.contrib import admin
 # Create your models here.
 class BlogsPost(models.Model):
-    title = models.CharField(max_length = 150)
+    title = models.CharField(max_length=150)
     body = models.TextField()
-    author = models.CharField(max_length = 50,default='Timi')
+    author = models.CharField(max_length=50, default='Timi')
     timestamp = models.DateTimeField()
     blog_id = models.IntegerField(default=0)
-    tag = models.CharField(max_length = 20,default='python')
+    tag = models.CharField(max_length=20, default='python')
 
 class BlogPostAdmin(admin.ModelAdmin):
-    list_display = ('title','timestamp','author','id')
+    list_display = ('title', 'timestamp', 'author', 'id')
 
-    class Media:
-        js = (
-            '/static/js/kindeditor-4.1.10/kindeditor-min.js',
-            '/static/js/kindeditor-4.1.10/lang/zh_CN.js',
-            '/static/js/kindeditor-4.1.10/config.js',
-        )
+    # class Media:
+    #     js = (
+    #         '/static/js/kindeditor-4.1.10/kindeditor-min.js',
+    #         '/static/js/kindeditor-4.1.10/lang/zh_CN.js',
+    #         '/static/js/kindeditor-4.1.10/config.js',
+    #     )
 
 admin.site.register(BlogsPost,BlogPostAdmin)
