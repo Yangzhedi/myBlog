ea6d120060eb3ccbbd0ed8f83c2729221e84141a
Yangzhedi <uiryzd@163.com>
2017-02-23 17:07:57 +0800

    股票爬取loading过渡完成

--- blog/static/js/app/spider/StockSpider.js
+++ blog/static/js/app/spider/StockSpider.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
 import stockCode from './stockCode'
 import SideBar from './SideBar'
+import AjaxFunction from '../util/AjaxFunction'
 'use strict';
+
+var linkLoading = false;
 class StockSpider extends Component{
     constructor(props){
         super(props);
@@ -10,6 +13,7 @@ class StockSpider extends Component{
             value: '',
             linkIsShow: false,
             linkContent: '',
+            stockContent:'',
             arr: []
         };
 
@@ -19,8 +23,9 @@ class StockSpider extends Component{
 
     };
 
+
     handleChange(e){
-        console.log(e.target.value);
+        // console.log(e.target.value);
         this.setState({
             // 去除空格
             value: e.target.value.replace(/\s/g, "").slice(0,6)
@@ -29,42 +34,51 @@ class StockSpider extends Component{
 
     handleClick(e){
         // console.log(e.target);
-        this.setState({
-            linkContent: ''
-        });
-        if(this.state.value.length == 6){
-            var data = {
-                value: this.state.value
-            };
-            $.ajax({
-                url:'/API/stock-code-search',
-                type : "GET",
-                data : data,
-                success:function(response,stutas,xhr){
-                    // console.log('response is ');
-                    // console.log(response);
-                    this.setState({
-                        linkContent: 'Download'+response+'.csv',
-                        linkIsShow: true
-                    })
-                }.bind(this)
-            });
-        }
-
-    }
-
-    handleSearch(){
-        // console.log(stockCode.allSHcode)
         var this_ = this;
         var bool = false;
-        var idx = 0;
+        var idx = null;
         stockCode.allSHcode.forEach(function(item,index) {
             if(item == this_.state.value){
                 bool = true;
                 idx = index;
             }
         });
+        this.setState({
+            stockContent:stockCode.allSHcodeWithName[idx]
+        });
+        if(idx !== null){
+            linkLoading = true;
+            this.setState({
+                linkContent: ''
+            });
+            if(this.state.value.length == 6){
+                var data = {
+                    value: this.state.value
+                };
+                AjaxFunction.ajax({
+                    url:'/API/stock-code-search',
+                    type : "GET",
+                    data : data,
+                    success:function(response,stutas,xhr){
+                        // console.log('response is ');
+                        // console.log(response);
+                        this.setState({
+                            linkContent: 'Download'+response+'.csv',
+                            linkIsShow: true
+                        });
+                        linkLoading = false;
+                    }.bind(this)
+                });
+            }
+        }
+        console.log(idx);
         console.log(stockCode.allSHcodeWithName[idx])
+
+
+    }
+
+    handleSearch(){
+        // console.log(stockCode.allSHcode)
     }
 
     componentWillMount(){
@@ -72,10 +86,14 @@ class StockSpider extends Component{
     }
 
     render() {
+        var stockContent = this.state.stockContent ? "show": "hidden";
+        var stockLoading = linkLoading ? "show": "hidden";
         var stockLink = "stock-link";
         if(this.state.linkContent){
-            stockLink+="show";
+            stockLink += "show";
+            stockLoading = 'hidden';
         }
+        console.log(stockLoading);
         var downloadHref = '/data/'+this.state.value;
         return(
             <div className="row">
@@ -87,21 +105,23 @@ class StockSpider extends Component{
                                 return <span key={index}>{'\''+item+'\','}</span>
                             })
                         }
-                        <input type="text" value={this.state.value}
+                        <input type="number" value={this.state.value}
                            onChange={this.handleChange}/>
-                        <button className='spider-button btn-4'
-                                onClick={this.handleSearch}>验证</button>
-                        <button className='spider-button' disabled={!this.state.value}
-                                onClick={this.handleClick}>开始爬取</button>
+                        {/*<button className='spider-button btn-4'
+                                onClick={this.handleSearch}>验证</button>*/}
+                        <button className='spider-button btn-4' disabled={!this.state.value}
+                                onClick={this.handleClick}>爬取数据</button>
                     </div>
-
-                    <div className="loading">
-                        <p>正在查询股票代码</p>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
+                    <div className="" className={stockContent}>
+                        <p>已监测到股票：{this.state.stockContent}，开始为您爬取</p>
+                    </div>
+                    <div className={"loading "+stockLoading}>
+                        <p>正在爬取股票数据</p>
+                        <span> </span>
+                        <span> </span>
+                        <span> </span>
+                        <span> </span>
+                        <span> </span>
                     </div>
                     <a className={stockLink} href={downloadHref}>{this.state.linkContent}</a>
                 </div>
